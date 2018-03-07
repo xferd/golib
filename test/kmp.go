fd49e003b5cfbaea8b36493e23891bd89d1a3ff6
liyan34 <liyan34@lenovo.com>
2018-03-07 10:10:34 +0800

    kmp

--- test/kmp.go
+++ test/kmp.go
@@ -8,12 +8,9 @@ import (
 func getk(p string) (k []int) {
     k = make([]int, len(p))
     k[0] = 0
-    for i, c := range p {
-        if i == 0 {
-            continue
-        }
+    for i := 1; i < len(p); i++ {
         k[i] = k[i - 1]
-        if p[k[i - 1]] == uint8(c) {
+        if p[k[i - 1]] == p[i] {
             k[i]++
         } else {
             k[i] = 0
@@ -25,22 +22,26 @@ func getk(p string) (k []int) {
 
 func kmp(s, p string) int {
     k := getk(p)
+    fmt.Println("pattern: ", p)
     fmt.Println("k:", k)
-    for i := 0; i < len(s) - len(p); {
-        found := true
-        for j, c := range p {
-            fmt.Println("i:", i, "j:", j)
-            fmt.Println(s)
-            fmt.Println(strings.Repeat(" ", i + j) + "^")
-            fmt.Println(strings.Repeat(" ", i) + p)
-            fmt.Println(strings.Repeat("=", len(s)))
-            if c != rune(s[i + j]) {
-                found = false
-                i = i + j - k[j]
-                break
+    st := 0
+    for i, j := 0, 0; i < len(s) - len(p); {
+        st++
+        fmt.Printf("step %d -> i: %d, j: %d\n", st, i, j)
+        fmt.Println(s)
+        fmt.Println(strings.Repeat(" ", i + j) + "^")
+        fmt.Println(strings.Repeat(" ", i) + p)
+        fmt.Println(strings.Repeat("=", len(s)))
+        if s[i + j] == p[j] {
+            j++;
+        } else {
+            i = i + j - k[j]
+            if j = k[j]; j < 0 {
+                j = 0
             }
+            continue
         }
-        if found {
+        if j >= len(p) {
             return i
         }
     }
